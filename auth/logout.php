efbab5121ccc95ff5ed0c3042b619c007e9fb0f9
eduntoluwadylan-code <eduntoluwadylan@gmail.com>
2026-04-20 19:15:00 +0300

    Added recipe image upload support

--- auth/logout.php
+++ auth/logout.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 $token = $_POST['csrf_token'] ?? null;
 if (!cn_csrf_verify(is_string($token) ? $token : null)) {
-    cn_redirect('../index.php');
+    cn_redirect('../index.php'); 
 }
 
 cn_logout_user();
